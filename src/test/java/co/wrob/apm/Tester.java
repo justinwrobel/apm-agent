c5fcf7087346159d025ba1e7ae3b348a2c0e2aa9
Justin Wrobel <justin.wrobel@laureate.net>
2017-07-12 15:47:37 -0400

    added test to create modified string class

--- src/test/java/co/wrob/apm/Tester.java
+++ src/test/java/co/wrob/apm/Tester.java
@@ -1,9 +1,20 @@
 package co.wrob.apm;
 
+import java.util.Properties;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.junit.Test;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Tester {
+	
 	/*
 	public void test(){
 		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
@@ -35,22 +46,59 @@ public class Tester {
 	}
 	
 	*/
-	public void test2(){
-		ServletRequest req;
-		ServletResponse res;
+//	public void test2(){
+//		ServletRequest req;
+//		ServletResponse res;
+//		
+//		javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest) req;
+//		javax.servlet.http.HttpServletResponse response = (javax.servlet.http.HttpServletResponse) res;
+//		//response.setIntHeader(name, value);
+//		
+////		HttpServletRequest request = (HttpServletRequest)org.apache.catalina.core.ApplicationFilterChain.getLastServicedRequest();
+////		HttpServletResponse response = (HttpServletRequest)org.apache.catalina.core.ApplicationFilterChain.getLastServicedResponse();
+//		
+//		if(request.getAttribute("stringCount")==null){
+//			request.setAttribute("stringCount", 0);
+//		}else{
+//			int stringCount = 1 + ((int)request.getAttribute("String"));
+//			request.setAttribute("stringCount", stringCount);
+//		}
+//		
+//	}
+	
+	@Test
+	public void test3() throws Exception{
+		String RESOURCE_FILE = "java-strings.xml";
+		Properties properties;
 		
-		javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest) req;
-		javax.servlet.http.HttpServletResponse response = (javax.servlet.http.HttpServletResponse) res;
-		//response.setIntHeader(name, value);
 		
-//		HttpServletRequest request = (HttpServletRequest)org.apache.catalina.core.ApplicationFilterChain.getLastServicedRequest();
-//		HttpServletResponse response = (HttpServletRequest)org.apache.catalina.core.ApplicationFilterChain.getLastServicedResponse();
+		properties = new Properties();
+		properties.loadFromXML(getClass().getClassLoader().getResourceAsStream(RESOURCE_FILE));
+		addStringTransform();
 		
-		if(request.getAttribute("stringCount")==null){
-			request.setAttribute("stringCount", 0);
-		}else{
-			int stringCount = 1 + ((int)request.getAttribute("String"));
-			request.setAttribute("stringCount", stringCount);
+	}
+	
+	private void addStringTransform() {
+		log.debug("addStringTransform");
+		ClassPool classPool = ClassPool.getDefault();
+		
+		try {
+			
+			CtClass ctClass = classPool.get("java.lang.String");
+			log.debug(ctClass.getName());
+			
+			ctClass.defrost();
+			CtConstructor[] constructors = ctClass.getConstructors();
+			for (CtConstructor ctConstructor : constructors) {
+				log.debug("!!!" + ctConstructor);
+				ctConstructor.insertAfter("asdSystem.out.println(\"!!!instr!!!\");");
+			}
+			ctClass.writeFile(".");
+			ctClass.detach();
+			log.debug("detached");
+
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
 		
 	}
